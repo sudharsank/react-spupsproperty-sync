f071875818555db7df91f77ac265327b5e487940
Sudharsan K <sudharsan@o365practice.onmicrosoft.com>
2020-03-13 16:20:10 +0000

    Initial commit.

---
+++ src/webparts/spupsProperySync/components/ISpupsProperySyncProps.ts
@@ -0,0 +1,3 @@
+export interface ISpupsProperySyncProps {
+  description: string;
+}
---
+++ src/webparts/spupsProperySync/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpupsProperySyncWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpupsProperySyncWebPartStrings' {
+  const strings: ISpupsProperySyncWebPartStrings;
+  export = strings;
+}
